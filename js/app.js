a2bb60f2c9a7b2d0548f35b9bb427072c98b18f9
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-10-20 02:54:04 +0300

    match score and animations added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+let scoreBoard = document.querySelector("#scoreboard");
 let homeScore = document.querySelector("#homeScore");
 let awayScore = document.querySelector("#awayScore");
+const endGame = document.querySelector("#endGame");
+const newGame = document.querySelector("#newGame");
+let resultBoard = document.querySelector("#resultPage");
+let result = document.querySelector("#result");
 
 var home = 0;
 var away = 0;
@@ -14,3 +19,24 @@ const sumNumber = (field, count) => {
     awayScore.textContent = away;
   }
 };
+
+endGame.addEventListener("click", () => {
+  scoreBoard.classList.add("scoreBoardOff");
+  resultBoard.classList.remove("scoreBoardOn");
+  if (home > away) {
+    result.innerHTML = `Home won! Score: ${home} - ${away}`;
+  } else if (home == away) {
+    result.innerHTML = `It's a tie! Score: ${home} - ${away}`;
+  } else {
+    result.innerHTML = `Away won! Score: ${home} - ${away}`;
+  }
+});
+
+newGame.addEventListener("click", () => {
+  scoreBoard.classList.remove("scoreBoardOff");
+  resultBoard.classList.add("scoreBoardOn");
+  home = 0;
+  away = 0;
+  homeScore.textContent = home;
+  awayScore.textContent = away;
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  safelist: ["scoreBoardOff", "scoreBoardOnn"],
   theme: {
     fontFamily: {
       primaryFont: ["Lato", "sans-serif"],
